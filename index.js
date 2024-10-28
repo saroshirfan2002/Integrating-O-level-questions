7e1dd8cde8f2f38a5a058ec9734b7e2ff9dd0dfe
saroshirfan2002 <166515100+saroshirfan2002@users.noreply.github.com>
2024-10-28 19:37:25 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ChemistryQuestion from './App'; // Make sure this imports the right component
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(<ChemistryQuestion />);
